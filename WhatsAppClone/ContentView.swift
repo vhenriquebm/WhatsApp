12efc866cb32dffe6e2a1388ce459c8022a05516
vhenriquebm <vhenriquebm@gmail.com>
2024-09-12 18:37:47 -0300

    Initial Commit

---
+++ WhatsAppClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsAppClone
+//
+//  Created by Vitor Henrique Barreiro Marinho on 12/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneApp.swift
+//  WhatsAppClone
+//
+//  Created by Vitor Henrique Barreiro Marinho on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
